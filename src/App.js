a63f59927577b43eb2189212e415361544606ab4
rijan-1 <rijanrai125@gmail.com>
2024-01-19 22:12:47 +0000

    renuwed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import RegistrationForm from './Register';
+import LoginForm from './Login';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+
+    const getTest = async()=>{
+      const response = await fetch('https://localhost:8000')
+    }
+  })
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<RegistrationForm/>}/>
+        <Route path='/Login' element={<LoginForm/>}/>
+        
+        </Routes></BrowserRouter>
+ 
     </div>
   );
 }
